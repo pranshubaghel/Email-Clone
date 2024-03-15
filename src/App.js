7b727b26d48a1ae1ef83a44c38555ef073c3e390
pranshu baghel <devloperpranshu@gmail.com>
2024-03-15 13:56:44 +0530

    Email clone

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import InboxMsg from './Components/InboxMsg';
 import Header from './Components/Header';
 import LeftSideBar from './Components/LeftSideBar';
 import AppContainer from './Components/AppContainer';
@@ -12,6 +11,8 @@ import Drafts from './Components/Sub-components/Drafts';
 import Inbox from './Components/Sub-components/Inbox';
 
 
+
+
 function App() {
   return (
     <>
@@ -19,15 +20,15 @@ function App() {
       <div class="body-wrapper" >
 
         <Header />
-        <InboxMsg />
+        {/* <InboxMsg /> */}
         <LeftSideBar />
         <AppContainer /> 
          <Routes>
-        <Route path="/Inbox" element={<Inbox/>} />
-        <Route path="/Starred" element={<Starred/>} />
-        <Route path="/Snoozed" element={<Snoozed/>} />
-        <Route path="/Sent" element={<Sent/>} />
-        <Route path="/Drafts" element={<Drafts/>} />
+        <Route path="/inbox" element = {<Inbox/>}/>
+        <Route path="/starred" element={<Starred/>} />
+        <Route path="/snoozed" element={<Snoozed/>} />
+        <Route path="/sent" element={<Sent/>} />
+        <Route path="/drafts" element={<Drafts/>} />
         </Routes> 
         
 
