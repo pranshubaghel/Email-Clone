dbe4f47ffef1ffff746a32164b80eaa273e361cd
pranshu baghel <devloperpranshu@gmail.com>
2024-03-26 20:54:29 +0530

    fetch error

--- src/Components/Sub-components/Inbox.jsx
+++ src/Components/Sub-components/Inbox.jsx
@@ -99,23 +99,23 @@ setData(maildata)
 <div class="message-default" >
 
   <div class="message-sender message-content unread" >
-    <span >Cascadom</span>
+    <span >{value.payload.headers.find(item=>item.name == "From").value}</span>
   </div>
 
   <div class="message-subject message-content unread">
-    <span>Dev Horror Stories: 👻 2000 lines of inline styles</span>
+    <span>{value.payload.headers.find(item=>item.name == "Subject").value}</span>
   </div>
 
   <div class="message-seperator message-content"> - </div>
 
   <div class="message-body message-content">
-    <span> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta error beatae optio ea, quod harum. Iure ab sed, dolores eos repudiandae inventore magnam id modi blanditiis harum at. Facere, exercitationem.</span>
+    <span>{value.snippet}</span>
   </div>
 
   <div class="gap message-content" > &nbsp; </div>
 
   <div class="message-date center-text unread" >
-    <span>12:09 AM</span>
+    <span>{value.payload.headers.find(item=>item.name == "Date").value}</span>
   </div>
 
 </div>
@@ -161,119 +161,7 @@ setData(maildata)
     {/* <img src={ClassStarred}alt="Not starred" class="btn-icon-sm btn-icon-alt btn-icon-hover message-btn-icon" /> */}
   </button>
 </div>
-
-<div class="message-default" >
-
-  <div class="message-sender message-content" >
-    <span >Mr. President</span>
-  </div>
-
-  <div class="message-subject message-content">
-    <span>Thanks for Saving the World</span>
-  </div>
-
-  <div class="message-seperator message-content"> - </div>
-
-  <div class="message-body message-content">
-    <span> Party at my crib next weekend, amet consectetur adipisicing elit. Soluta error beatae optio ea, quod harum. Iure ab sed, dolores eos repudiandae inventore magnam id modi blanditiis harum at. Facere, exercitationem.</span>
-  </div>
-
-  <div class="gap message-content" > &nbsp; </div>
-
-  <div class="message-date center-text" >
-    <span>4:23 PM</span>
-  </div>
-
 </div>
-
-<div class="message-group-hidden" >
-  <div class="inbox-message-item-options">
-    <button class="btn">
-      {/* <img src={ClassArchive}alt="Archive" class="btn-icon-sm btn-icon-alt btn-icon-hover" /> */}
-    </button>
-
-    <button class="btn">
-      {/* <img src={DeleteBtn}alt="Delete" class="btn-icon-sm btn-icon-alt btn-icon-hover" /> */}
-    </button>
-
-    <button class="btn">
-      {/* <img src={MarkRead} alt="Mark as unread" class="btn-icon-sm btn-icon-alt btn-icon-hover" /> */}
-    </button>
-
-    <button class="btn">
-      {/* <img src={SnoozeBtn} alt="Snooze" class="btn-icon-sm btn-icon-alt btn-icon-hover" /> */}
-    </button>
-  </div>
-</div>
-
-</div>
-
-<div class="inbox-message-item  message-default-unread">
-
-<div class="checkbox">
-  <button class="btn">
-    {/* <img src={SelectedBtn} alt="Select" class="btn-icon-sm btn-icon-alt btn-icon-hover message-btn-icon" /> */}
-  </button>
-</div>
-
-<div class="message-group-hidden">
-  <button class="btn-alt btn-nofill drag-indicator" >
-    {/* <img src={DragBtn} alt="Drag" class="btn-icon-sm btn-icon-alt btn-icon-disabled" /> */}
-  </button>
-</div>
-
-<div >
-  <button class="btn star">
-    {/* <img src={Astarred} alt="Not starred" class="btn-icon-sm btn-icon-alt btn-icon-hover message-btn-icon" /> */}
-  </button>
-</div>
-
-<div class="message-default" >
-
-  <div class="message-sender message-content" >
-    <span >{value.snippet}</span>
-  </div>
-
-  <div class="message-subject message-content">
-    <span></span>
-  </div>
-
-  <div class="message-seperator message-content"> - </div>
-
-  <div class="message-body message-content">
-    <span></span>
-  </div>
-
-  <div class="gap message-content" > &nbsp; </div>
-
-  <div class="message-date center-text" >
-    <span></span>
-  </div>
-
-</div>
-
-<div class="message-group-hidden" >
-  <div class="inbox-message-item-options">
-    <button class="btn">
-      {/* <img src={BtnArchie} alt="Archive" class="btn-icon-sm btn-icon-alt btn-icon-hover" /> */}
-    </button>
-
-    <button class="btn">
-      {/* <img src={BtnDelet}alt="Delete" class="btn-icon-sm btn-icon-alt btn-icon-hover" /> */}
-    </button>
-
-    <button class="btn">
-      {/* <img src={BtnUnread}alt="Mark as unread" class="btn-icon-sm btn-icon-alt btn-icon-hover" /> */}
-    </button>
-
-    <button class="btn">
-      {/* <img src={Snooje} alt="Snooze" class="btn-icon-sm btn-icon-alt btn-icon-hover" /> */}
-    </button>
-  </div>
-</div>
-
-</div>
-
 </>))}         
 </>
     );
